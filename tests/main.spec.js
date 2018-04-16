a2f901bdbe62f997c8c44afe17e3ca718c70005c
Édipo Juan <edipojs@gmail.com>
2018-04-16 00:42:31 -0400

    Foi implementado os 'smoke tests'

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,3 +1,46 @@
 import { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
-describe('Main', () => {});
+describe('Spotify Wrapper', () => {
+  describe('smoke tests', () => {
+    it('deve existir o método search', () => {
+      expect(search).to.not.undefined;
+    });
+
+    it('deve retornar um array quando executar o método search', () => {
+      expect(search()).to.be.a('array');
+    });
+
+    it('deve existir o método searchAlbums', () => {
+      expect(searchAlbums).to.not.undefined;
+    });
+
+    it('deve retornar um array quando executar o método searchAlbums', () => {
+      expect(searchAlbums()).to.be.a('array');
+    });
+
+    it('deve existir o método searchArtists', () => {
+      expect(searchArtists).to.not.undefined;
+    });
+
+    it('deve retornar um array quando executar o método searchArtists', () => {
+      expect(searchArtists()).to.be.a('array');
+    });
+
+    it('deve existir o método searchTracks', () => {
+      expect(searchTracks).to.not.undefined;
+    });
+
+    it('deve retornar um array quando executar o método searchTracks', () => {
+      expect(searchTracks()).to.be.a('array');
+    });
+
+    it('deve existir o método searchPlaylists', () => {
+      expect(searchPlaylists).to.not.undefined;
+    });
+
+    it('deve retornar um array quando executar o método searchPlaylists', () => {
+      expect(searchPlaylists()).to.be.a('array');
+    });
+  });
+});
